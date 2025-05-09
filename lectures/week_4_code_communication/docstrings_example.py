117d101570e2b53729f1fe1c815d5b73e4521b15
Jakob Damen <jakob.damen@outlook.com>
2025-05-09 11:23:58 +0200

    Added lecture 4 and post readings and watching of week 4

---
+++ lectures/week_4_code_communication/docstrings_example.py
@@ -0,0 +1,14 @@
+"""I'm a module docstring."""
+
+
+def example_function():
+    """I'm a function docstring."""
+    pass
+
+
+class ExampleClass:
+    """I'm a class docstring."""
+
+    def __init__(self):
+        """Hello, I'm a method docstring."""
+        pass
